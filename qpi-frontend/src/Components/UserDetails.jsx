535124c8e412168274ff2eba6bc06d0663890b7f
Shashank Yelluri <104439629+shashank-yelluri@users.noreply.github.com>
2023-02-24 20:09:18 +0530

    Add project board

--- qpi-frontend/src/Components/UserDetails.jsx
+++ qpi-frontend/src/Components/UserDetails.jsx
@@ -4,18 +4,21 @@ import { createTask, getTasks, getUserById } from "../api/api";
 import { Task } from "./Task";
 import {
   LogoutOutlined,
+  MenuOutlined,
   PlusOutlined,
   SettingOutlined,
 } from "@ant-design/icons";
-import { Divider, Modal } from "antd";
+import { Avatar, Badge, Divider, Modal, Segmented } from "antd";
+import { useEffect, useState } from "react";
 import { AddTask } from "./AddTask";
-import { useState } from "react";
 
 export const UserDetails = () => {
   let { userId: id } = useParams();
   const navigate = useNavigate();
   const queryClient = useQueryClient();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [segmentValue, setSegmentValue] = useState("Apps");
+  const [board, setBoard] = useState(false);
 
   const {
     isError: userIsError,
@@ -40,6 +43,11 @@ export const UserDetails = () => {
     setIsModalOpen(false);
   };
 
+  useEffect(() => {
+    setBoard(!board);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [segmentValue]);
+
   if (userIsLoading) return <h2>Loading...</h2>;
   if (userIsError) return <p>{JSON.stringify(userError)}</p>;
 
@@ -55,6 +63,9 @@ export const UserDetails = () => {
   const handleLogout = () => {
     navigate("/");
   };
+
+  let devopsCount = data.filter((task) => task?.board === "DevOps").length;
+  let appsCount = data.length - devopsCount;
 
   return (
     <div
@@ -98,6 +109,24 @@ export const UserDetails = () => {
 
       <div
         style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Segmented
+          options={["Apps", "DevOps"]}
+          value={segmentValue}
+          onChange={setSegmentValue}
+        />
+
+        <Badge count={board ? appsCount : devopsCount}>
+          <Avatar shape="square" size="small" icon={<MenuOutlined />} />
+        </Badge>
+      </div>
+
+      <div
+        style={{
           height: "200px",
           marginTop: "24px",
           padding: "12px",
@@ -105,14 +134,32 @@ export const UserDetails = () => {
         }}
       >
         {data &&
-          data.map((task) => (
-            <Task
-              task={task}
-              key={task._id}
-              userName={name}
-              isAdmin={isAdmin}
-            />
-          ))}
+          // eslint-disable-next-line array-callback-return
+          data.map((task) => {
+            if (segmentValue === "DevOps") {
+              if (task?.board === "DevOps") {
+                return (
+                  <Task
+                    task={task}
+                    key={task._id}
+                    userName={name}
+                    isAdmin={isAdmin}
+                  />
+                );
+              }
+            } else {
+              if (task?.board !== "DevOps") {
+                return (
+                  <Task
+                    task={task}
+                    key={task._id}
+                    userName={name}
+                    isAdmin={isAdmin}
+                  />
+                );
+              }
+            }
+          })}
       </div>
 
       <Modal
