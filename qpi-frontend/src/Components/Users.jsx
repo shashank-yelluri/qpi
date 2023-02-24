85e4268ae44c7c4d4e3d3834e99c77d013853ea7
Shashank Yelluri <104439629+shashank-yelluri@users.noreply.github.com>
2023-02-24 20:09:46 +0530

    Add project board

--- qpi-frontend/src/Components/Users.jsx
+++ qpi-frontend/src/Components/Users.jsx
@@ -2,9 +2,10 @@ import {
   ArrowLeftOutlined,
   UserAddOutlined,
   UsergroupAddOutlined,
+  UserOutlined,
 } from "@ant-design/icons";
-import { Divider, Modal } from "antd";
-import { useState } from "react";
+import { Avatar, Badge, Divider, Modal, Segmented } from "antd";
+import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
 import { getUsers } from "../api/api";
@@ -16,6 +17,8 @@ export const Users = () => {
   const { isError, error, isLoading, data } = useQuery(["users"], getUsers);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isUploadModelOpen, setIsUploadModelOpen] = useState(false);
+  const [segmentValue, setSegmentValue] = useState("Users");
+  const [admin, setAdmin] = useState(false);
   const navigate = useNavigate();
 
   const showModal = () => {
@@ -34,8 +37,16 @@ export const Users = () => {
     setIsUploadModelOpen(false);
   };
 
+  useEffect(() => {
+    setAdmin(!admin);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [segmentValue]);
+
   if (isLoading) return <h2>Loading...</h2>;
   if (isError) return <p>{JSON.stringify(error)}</p>;
+
+  let usersCount = data.filter((user) => !user.isAdmin).length;
+  let adminsCount = data.filter((user) => user.isAdmin).length;
 
   return (
     <div
@@ -76,13 +87,38 @@ export const Users = () => {
 
       <div
         style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Segmented
+          options={["Users", "Admins"]}
+          value={segmentValue}
+          onChange={setSegmentValue}
+        />
+        <Badge count={admin ? usersCount : adminsCount}>
+          <Avatar shape="square" size="small" icon={<UserOutlined />} />
+        </Badge>
+      </div>
+
+      <div
+        style={{
           height: "200px",
           marginTop: "32px",
           overflowY: "scroll",
           padding: "12px",
         }}
       >
-        {data && data.map((user) => <User key={user._id} user={user} />)}
+        {data &&
+          // eslint-disable-next-line array-callback-return
+          data.map((user) => {
+            if (segmentValue === "Users") {
+              if (!user.isAdmin) return <User key={user._id} user={user} />;
+            } else {
+              if (user.isAdmin) return <User key={user._id} user={user} />;
+            }
+          })}
       </div>
 
       <Modal
